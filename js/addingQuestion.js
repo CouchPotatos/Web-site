ff2993762eb3bca257013619c0bcccfcf1fd2e2e
rsp243 <reshetov.semjon@gmail.com>
2021-03-07 21:43:27 +0400

    added Выбрано

--- js/addingQuestion.js
+++ js/addingQuestion.js
@@ -3,8 +3,6 @@ let lengthOfSelectMenu = 0;
 
 var saveBtn = $('#saveBtn')
 
-
-
 $.ajax({
 	type: 'GET',
 	url: "https://api-test-post.herokuapp.com/api/v1/answers/",
@@ -12,35 +10,29 @@ $.ajax({
   	success: function(data){
   		for (i; i < data.length; i++) {
   			var newClass;
-  			if (i){
-  				var cloned = $('tbody > .cont').clone().prependTo('.ListAnsw');
-				
-  				newClass = 'cont' + i;
-  				cloned.removeClass('cont').addClass(newClass);
-				$("." + newClass + " div").attr("id", "app2");
-				let app = new Vue({
-					el: "#app2",
-					data: {
-						picked: false
-					}
-				});  
-			}
+  			
+  			var cloned = $('tbody > .cont').clone().prependTo('.ListAnsw');
+  			newClass = 'cont' + i;
+  			cloned.removeClass('cont').addClass(newClass).css("display", "");
+			$("." + newClass + " div").attr("id", "app2");
+
+			let app = new Vue({
+				el: "#app2",
+				data: {
+					picked: false
+				}
+			});  
+			
   			var textQuest = data[i]['text'];
   			var textBefore = data[i]['message_before_question'];
   			let goto = data[i]['goto'];
-  			if (i){
-  				$('.' + newClass + ' .text').text(textQuest);	
-    			$('.' + newClass + ' .before').text(textBefore);	
-    			$('.' + newClass + ' .textQuest').text(goto);
-  			} else {
-  				$('.cont .text').text(textQuest);	
-    			$('.cont .before').text(textBefore);	
-    			$('.cont .textQuest').text(goto);   
-  			}
+  			$('.' + newClass + ' .text').text(textQuest);	
+    		$('.' + newClass + ' .before').text(textBefore);	
+    		$('.' + newClass + ' .textQuest').text(goto);
 		}
     }
 });
-	
+
 
 
 /*
