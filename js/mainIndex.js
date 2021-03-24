ecb8ba6c29d2de742f4f6e12a5e59ebaed2017a0
rsp243 <you@example.comreshetov.semjon@gmail.com>
2021-03-24 23:50:14 +0400

    Added editQuestForm

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -5,23 +5,56 @@ $.ajax({
 	url: "https://api-test-post.herokuapp.com/api/v1/questions/",
 	dataType: 'json',  	
   	success: function(data){
+		let textArr = [];
+		let messageBefore = [];
+		let answList = [];
   		for (let i = 0; i < data.length; i++) {
+			textArr.push(data[i]['text']);
+			messageBefore.push(data[i]['message_before_question']);
+			answList.push(data[i]['answers']);
   			var newClass;
 			let newId;
   			var cloned = $('tbody .cont').clone().appendTo('.main');
   			newClass = 'cont' + i;
 			newId = 'popedUp' + i;
 			cloned.removeClass('cont').addClass(newClass).css("display", "");
+			newId = 'popedUp' + i;
 			$('.' + newClass + ' form #popedUp').attr('id', newId);
-
-			$('#' + newId).on('click', function(){
-				$('html').css("overflow", "hidden");
-			});
-			
   			var textQuest = data[i]['text'];
   			var textBefore = data[i]['message_before_question'];
   			$('.' + newClass + ' .text').text(textQuest);	
     		$('.' + newClass + ' .before').text(textBefore);
+
+			$('#' + newId).on('click', function(){
+				$('html').css("overflow", "hidden");
+				$('#textQ').text(textArr[Number(newId.slice(7))]);
+				$('#beforeQ').text(messageBefore[Number(newId.slice(7))]);
+				let height = 350;
+				for (let i = 0; i < answList[Number(newId.slice(7))].length; i++){
+					let question;
+					question = answList[Number(newId.slice(7))][i];
+					console.log(question);
+					$.ajax({
+						type: 'GET',
+						url: "https://api-test-post.herokuapp.com/api/v1/answer/" + question,
+						dataType: 'json',  	
+						  	success: function(data){
+								console.log(data['text']);
+								let cloned = $('.listAnsw .contAnsw').clone().appendTo('.listAnsw');
+								newClass = 'contAnsw' + i;
+								cloned.removeClass('contAnsw').addClass(newClass).css("display", "");
+								let textAnsw = data['text'];
+  								let nextQuest = data['goto'];
+  								$('.' + newClass + ' .textAnsw').text(textAnsw);	
+    							$('.' + newClass + ' .goto').text(nextQuest);
+								height += 50;
+								$('#okno').css("height", String(height) + "px");
+						  	}
+					});
+				}
+				/*height += 50;
+				$('#okno').css("height", String(height) + "px");*/
+			});
 		}	
     }
 });
