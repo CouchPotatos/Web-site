01dde8c38eb45f200aedcad29a00009ef54e587d
rsp243 <reshetov.semjon@gmail.com>
2021-03-27 12:12:01 +0400

    Norm upload questionList to site

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -9,70 +9,76 @@ $.ajax({
 		let messageBefore = [];
 		let answList = [];
   		for (let i = 0; i < data.length; i++) {
-			textArr.push(data[i]['text']);
-			messageBefore.push(data[i]['message_before_question']);
-			answList.push(data[i]['answers']);
-  			var newClass;
-			let newId, newIdDelete;
-  			var cloned = $('tbody .cont').clone().appendTo('.main');
-  			newClass = 'cont' + i;
-			newId = 'popedUp' + i;
-			newIdDelete = 'popedUpDelete' + i;
-			cloned.removeClass('cont').addClass(newClass).css("display", "");
-			newId = 'popedUp' + i;
-			$('.' + newClass + ' form #popedUp').attr('id', newId);
-			$('.' + newClass + ' form #popedUpDelete').attr('id', newIdDelete);
-  			var textQuest = data[i]['text'];
-  			var textBefore = data[i]['message_before_question'];
-  			$('.' + newClass + ' .text').text(textQuest);	
-    		$('.' + newClass + ' .before').text(textBefore);
+			for (let j = 0; j <  data.length; j++){
+				if (i + 1 === data[j]['id']){
+					textArr.push(data[j]['text']);
+					messageBefore.push(data[j]['message_before_question']);
+					answList.push(data[j]['answers']);
+					var newClass;
+					let newId, newIdDelete;
+					var cloned = $('tbody .cont').clone().appendTo('.main');
+					newClass = 'cont' + i;
+					newId = 'popedUp' + i;
+					newIdDelete = 'popedUpDelete' + i;
+					cloned.removeClass('cont').addClass(newClass).css("display", "");
+					newId = 'popedUp' + i;
+					$('.' + newClass + ' form #popedUp').attr('id', newId);
+					$('.' + newClass + ' form #popedUpDelete').attr('id', newIdDelete);
+					var textQuest = data[j]['text'];
+					var textBefore = data[j]['message_before_question'];
+					$('.' + newClass + ' .text').text(textQuest);	
+					$('.' + newClass + ' .before').text(textBefore);
 
-			$('#' + newId).on('click', function(){
-				$('html').css("overflow", "hidden");
-				$('#textQ').text(textArr[Number(newId.slice(7))]);
-				$('#beforeQ').text(messageBefore[Number(newId.slice(7))]);
-				let height = 350;
-				for (let i = 0; i < answList[Number(newId.slice(7))].length; i++){
-					let question;
-					question = answList[Number(newId.slice(7))][i];
-					$.ajax({
-						type: 'GET',
-						url: "https://api-test-post.herokuapp.com/api/v1/answer/" + question,
-						dataType: 'json',  	
-						  	success: function(data){
-								let cloned = $('.listAnsw .contAnsw').clone().appendTo('.listAnsw');
-								newClass = 'contAnsw' + i;
-								cloned.removeClass('contAnsw').addClass(newClass).css("display", "");
-								let textAnsw = data['text'];
-  								let nextQuest = data['goto'];
-  								$('.' + newClass + ' .textAnsw').text(textAnsw);	
-    							$('.' + newClass + ' .goto').text(nextQuest);
-								height += 50;
-								$('#okno').css("height", String(height) + "px");
-						  	}
-					});
-				}
-			});
-
-			$('#' + newIdDelete).on('click', function(){
-				$('.cont' + i).css("background-color", "#D3D3D3");
-				$('html').css("overflow", "hidden");
-				$('#closePopUpDelete1').on('click', function(){
-					$.ajax({
-						type: 'DELETE',
-						url: 'http://127.0.0.1:8000/api/v1/question/' + String(i + 1) +'/delete',
-						success: function(result) {
-							if (result['failure'] === "undefined"){
-								alert(result['success']);
-							} else { 
-								alert(result['failure']);
-							}
-							window.location.href = "main.html";
+					$('#' + newId).on('click', function(){
+						$('html').css("overflow", "hidden");
+						$('#textQ').text(textArr[Number(newId.slice(7))]);
+						$('#beforeQ').text(messageBefore[Number(newId.slice(7))]);
+						let height = 350;
+						for (let i = 0; i < answList[Number(newId.slice(7))].length; i++){
+							let question;
+							question = answList[Number(newId.slice(7))][i];
+							$.ajax({
+								type: 'GET',
+								url: "https://api-test-post.herokuapp.com/api/v1/answer/" + question,
+								dataType: 'json',  	
+									success: function(data){
+										let cloned = $('.listAnsw .contAnsw').clone().appendTo('.listAnsw');
+										newClass = 'contAnsw' + i;
+										cloned.removeClass('contAnsw').addClass(newClass).css("display", "");
+										let textAnsw = data['text'];
+										let nextQuest = data['goto'];
+										$('.' + newClass + ' .textAnsw').text(textAnsw);	
+										$('.' + newClass + ' .goto').text(nextQuest);
+										height += 50;
+										$('#okno').css("height", String(height) + "px");
+									}
+							});
 						}
 					});
-					
-				});
-			});
+
+					$('#' + newIdDelete).on('click', function(){
+						$('.cont' + i).css("background-color", "#D3D3D3");
+						$('html').css("overflow", "hidden");
+						$('#closePopUpDelete1').on('click', function(){
+							$.ajax({
+								type: 'DELETE',
+								url: 'http://127.0.0.1:8000/api/v1/question/' + String(i + 1) +'/delete',
+								success: function(result) {
+									if (result['failure'] === "undefined"){
+										alert(result['success']);
+									} else { 
+										alert(result['failure']);
+									}
+									window.location.href = "main.html";
+								}
+							});
+							
+						});
+					});
+				}
+			}
+			
+
 		}	
     }
 });
