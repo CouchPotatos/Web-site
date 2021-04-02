2e0617eadf483508fc66244e930a594e4ca9a72e
rsp243 <reshetov.semjon@gmail.com>
2021-04-02 16:34:25 +0400

    Before conferation

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -9,17 +9,41 @@ $.ajax({
 		let messageBefore = [];
 		let answList = [];
 		let maxID = 0;
+		let idList = [];
+		let k = 1;
 		for (let i = 0; i < data.length; i++){
 			if (maxID < data[i]['id']){
 				maxID = data[i]['id'];
+				
 			}
+			while (k != data[i]['id']){
+				idList.push(-1);
+				k++;
+			}
+			if (k === data[i]['id']){
+				idList.push(data[i]['id']);
+				k++;
+			} 	
+		}
+		let r = 0;
+		for (let j = 0; j < idList.length; j++){
+			if (idList[j] != -1){
+				textArr.push(data[r]['text']);
+				messageBefore.push(data[r]['message_before_question']);
+				answList.push(data[r]['answers']);
+				r++;
+			} else {
+				textArr.push('-1');
+				messageBefore.push('-1');
+				answList.push([-1, -1]);	
+			}	
 		}
 		for (let i = 0; i < maxID; i++) {
 			for (let j = 0; j < data.length; j++){
 				if (i + 1 === data[j]['id']){
 					textArr.push(data[j]['text']);
 					messageBefore.push(data[j]['message_before_question']);
-					answList.push(data[j]['answers']);
+					answList.push(data[j]['answers']);	
 					var newClass;
 					let newId, newIdDelete;
 					var cloned = $('tbody .cont').clone().appendTo('.main');
@@ -39,7 +63,23 @@ $.ajax({
 						$('html').css("overflow", "hidden");
 						$('#textQ').text(textArr[Number(newId.slice(7))]);
 						$('#beforeQ').text(messageBefore[Number(newId.slice(7))]);
-						let height = 350;
+						let height = 460;
+						$.ajax({
+							type: 'GET',
+							url: "https://api-test-post.herokuapp.com/api/v1/answers/",
+							dataType: 'json',  
+							success: function(data){
+								let value = 0;
+								for (i; i < data.length; i++) {
+									var textAnsw = data[i]['text'];
+									newClass = 'menu' + value;
+   									value += 1;
+									var element = $('#floatingSelect');
+									var cloned = $('.menu').clone().appendTo(element);
+									cloned.removeAttr('value').attr({'value': value + 1}).text(textAnsw).removeClass('menu').addClass(newClass).css("display", "");
+								}	
+							}
+						})	
 						for (let i = 0; i < answList[Number(newId.slice(7))].length; i++){
 							let question;
 							question = answList[Number(newId.slice(7))][i];
@@ -55,7 +95,7 @@ $.ajax({
 										let nextQuest = data['goto'];
 										$('.' + newClass + ' .textAnsw').text(textAnsw);	
 										$('.' + newClass + ' .goto').text(nextQuest);
-										height += 50;
+										height += Number($('.' + newClass).height());
 										$('#okno').css("height", String(height) + "px");
 									}
 							});
@@ -86,6 +126,48 @@ $.ajax({
 		}	
     }
 });
+
+
+$.ajax({
+	type: 'GET',
+	url: "https://api-test-post.herokuapp.com/api/v1/answers/",
+	dataType: 'json',  	
+  	success: function(data){
+		request = data;
+		let i = 0;
+  		for (i; i < data.length; i++) {
+  			let newClass;
+  			let cloned = $('tbody > .contAnswer').clone().prependTo('.ListAnswers');
+  			newClass = 'contAnswer' + i;
+  			cloned.removeClass('contAnswer').addClass(newClass).css("display", "");			
+  			let textAnswer = data[i]['text'];
+  			let nextQuestion = data[i]['goto'];
+  			$('.' + newClass + ' .textAnswer').text(textAnswer);	
+    		$('.' + newClass + ' .nextQuest').text(nextQuestion);
+			
+    	}
+		$.ajax({
+			type: 'GET',
+			url: "https://api-test-post.herokuapp.com/api/v1/questions/",
+			dataType: 'json',  	
+			success: function(data){
+				for (let x = 0; x < i; x++){
+					let newClass;
+					newClass = 'contAnswer' + x;
+					for (let j = 0; j < data.length; j++){
+						if ($('.' + newClass + ' .nextQuest').text() == data[j]['id']){
+							$('.' + newClass + ' .nextQuest').text(data[j]['text'])
+						}
+					}
+				}
+			}
+		})
+	}
+});
+
+
+
+
 
 $('#popedUp').on('click', function(){
 	$('html').css("overflow", "hidden");
