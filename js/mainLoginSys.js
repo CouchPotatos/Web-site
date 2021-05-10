a9a205a481fd65dd6bc5bd0187edb2ca1c0d30c6
rsp243 <reshetov.semjon@gmail.com>
2021-05-10 15:58:44 +0400

    last-version

--- js/mainLoginSys.js
+++ js/mainLoginSys.js
@@ -1,13 +1,25 @@
 let flag = true
+
+let debug = 1;
+let logger = function(a,b) { if ( debug == 1 ) console.log(a, b || "")};
+
+logger("my", "log");
+
 $('.btn').on('click', function() {
     var login = document.getElementById('login').value
     var password = document.getElementById('password').value
-    if (login === 'admin' && password === 'SherBot'){
+    $.ajax({
+        type: 'POST',
+        url: 'https://last-api.herokuapp.com/auth/token/login',
+        data: { "password": password, "username": login},
+        success: function () {
             window.location.href = "main.html";
-        } else {
+        },
+        error: function() {
             if (flag){
                 $('.d-grid').prepend('<p style="border: 1px solid red; padding: 5px;">Убедитесь, что вы правильно ввели логин и пароль.<p>') 
                 flag = false
-            }
+            }  
         }
+    })
 })
